243a5cff16bc6fbc94ee596b6d1e5879b897b297
Constantine Povietkin <kpovetkin@continuuity.com>
2014-10-08 12:27:09 +0300

    [cyb-59] removed wrong grunt dependency

--- cdap-authentication-clients/javascript/Gruntfile.js
+++ cdap-authentication-clients/javascript/Gruntfile.js
@@ -74,5 +74,4 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-open');
 };
